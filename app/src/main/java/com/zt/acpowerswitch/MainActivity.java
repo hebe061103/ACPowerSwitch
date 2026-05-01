8be7bf7909b80d670560b077f468024d56168338
hebe061103 <109753786@qq.com>
2026-05-01 13:40:32 +0800

    光伏市电切换系统

--- app/src/main/java/com/zt/acpowerswitch/MainActivity.java
+++ app/src/main/java/com/zt/acpowerswitch/MainActivity.java
@@ -940,16 +940,14 @@ public class MainActivity extends AppCompatActivity{
     @SuppressLint("DefaultLocale")
     private void pro_date_power_data(ArrayList<BarEntry> barChart, String label, String des, String type) {
         //X轴设置显示位置在底部
+        power_chart.fitScreen();
         XAxis xAxis = power_chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         YAxis leftAxis = power_chart.getAxisLeft();//左侧Y轴保留两位小数
         leftAxis.setValueFormatter((value, axis) -> {
             return String.format("%.2f", value); // value 就是坐标轴上的数值
         });
-        YAxis rightAxis = power_chart.getAxisRight();//右侧Y轴保留两位小数
-        rightAxis.setValueFormatter((value, axis) -> {
-            return String.format("%.2f", value); // value 就是坐标轴上的数值
-        });
+        power_chart.getAxisRight().setEnabled(false);
         switch (type) {
             case "小时":
                 xAxis.setAxisMinimum(-0.5f);
@@ -967,30 +965,38 @@ public class MainActivity extends AppCompatActivity{
                 power_chart.getAxisLeft().setAxisMinimum(0f);//左侧Y轴最小值
                 break;
             case "年份":
-                xAxis.setAxisMinimum(2024-0.5f);
-                xAxis.setAxisMaximum(2054.5f);
+                xAxis.setAxisMinimum(2025-0.5f);
+                xAxis.setAxisMaximum(2055+0.5f);
                 power_chart.getAxisLeft().setAxisMinimum(0f);//左侧Y轴最小值
                 break;
         }
         xAxis.setGranularity(1f);
         BarData barData = getBarData(barChart, label);
-        barData.setValueFormatter((value, entry, dataSetIndex, viewPortHandler) -> {
-            return String.format("%.2f", value); // value 就是该柱子的实际数值
-        });
+        barData.setBarWidth(0.9f);
         power_chart.getDescription().setText(des);//右下角描述
         power_chart.getDescription().setTextSize(9f);
         power_chart.setData(barData);//调置数据
-        power_chart.setDoubleTapToZoomEnabled(false);
+        power_chart.setDoubleTapToZoomEnabled(false);// 禁用双击缩放
         power_chart.setScaleXEnabled(true); // 允许水平缩放（或设为 false 仅允许滑动）
         power_chart.setScaleYEnabled(false); // 禁止垂直缩放，防止 Y 轴乱跳
         power_chart.setDragEnabled(true); // 必须开启，否则无法滑动查看后面的数据
+        if (type.equals("小时")) {
+            power_chart.setVisibleXRangeMaximum(18f);
+            float maxX = barData.getXMax(); // 获取当前数据的最大 X 值
+            power_chart.moveViewToX(maxX); // 滚动到最新数据,如果数据超过 18 个，它会自动靠右；如果不足 18 个，它会停在左侧
+        }
+        if (type.equals("年份")) {
+            power_chart.setVisibleXRangeMaximum(18f);
+            float maxX = barData.getXMax(); // 获取当前数据的最大 X 值
+            power_chart.moveViewToX(maxX); // 滚动到最新数据,如果数据超过 18 个，它会自动靠右；如果不足 18 个，它会停在左侧
+        }
+        power_chart.invalidate(); // 最后统一刷新
     }
 
     @NonNull
     private static BarData getBarData(ArrayList<BarEntry> barChart, String label) {
         BarDataSet dataSet = new BarDataSet(barChart, label);
-        dataSet.setValueFormatter((value, entry, dataSetIndex, viewPortHandler) -> String.format(Locale.getDefault(), "%.1f", value));// 自定义值格式
-        dataSet.setBarBorderWidth(0.2f); // 设置条形图之间的间距
+        dataSet.setValueFormatter((value, entry, dataSetIndex, viewPortHandler) -> String.format(Locale.getDefault(), "%.2f", value));// 自定义值格式
         dataSet.setColor(Color.GREEN); // 设置柱子的颜色
         dataSet.setDrawValues(true); //是否绘制柱状图顶部的数值
         dataSet.setValueTextSize(8f);
